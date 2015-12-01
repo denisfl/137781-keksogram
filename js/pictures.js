39fada5186cb5756135f089e88d52a70edb31d52
denisfl <delchyve@gmail.com>
2015-12-01 22:35:06 +0300

    wip module5-task1

--- js/pictures.js
+++ js/pictures.js
@@ -1,12 +1,76 @@
-/* global pictures: true */
 'use strict';
 (function() {
   var container = document.querySelector('.pictures');
+  var pictures = [];
+  var filters = document.querySelector('.filters');
+  var activeFilter;
+  var filteredPictures = [];
 
-  pictures.forEach(function(picture) {
-    var element = getElemtentFromTemplate(picture);
-    container.appendChild(element);
+  filters.addEventListener('click', function(event) {
+    var clickedElement = event.target;
+    if (clickedElement.classList.contains('filters-item')) {
+      setActiveFilter(clickedElement.control.id);
+    }
   });
+
+  function getPictures() {
+    var xhr = new XMLHttpRequest();
+    container.classList.add('pictures-loading');
+
+    xhr.open('GET', './data/pictures.json');
+    xhr.onload = function(event) {
+      var data = event.target.response;
+      var loadedPictures = JSON.parse(data);
+      container.classList.remove('pictures-loading');
+      updatedLoadedPictures(loadedPictures);
+    };
+    xhr.send();
+  }
+
+  getPictures();
+
+  function updatedLoadedPictures(loadedPictures) {
+    pictures = loadedPictures;
+
+    setActiveFilter(activeFilter);
+  }
+
+  function setActiveFilter(id) {
+    if (activeFilter === id) {
+      return;
+    }
+
+    filteredPictures = pictures.slice(0);
+
+    switch (id) {
+      case 'filter-popular':
+        filteredPictures = filteredPictures.sort(function(a, b) {
+          return b.like - a.like;
+        });
+        break;
+      case 'filter-new':
+        filteredPictures = filteredPictures.sort(function(a, b) {
+          return new Date(b.date) - new Date(a.date);
+        });
+        break;
+      case 'filter-discussed':
+        filteredPictures = filteredPictures.sort(function(a, b) {
+          return b.comments - a.comments;
+        });
+        break;
+    }
+    renderPictures(filteredPictures);
+    activeFilter = id;
+  }
+
+  function renderPictures() {
+    container.innerHTML = '';
+
+    filteredPictures.forEach(function(picture) {
+      var element = getElemtentFromTemplate(picture);
+      container.appendChild(element);
+    });
+  }
 
   function getElemtentFromTemplate(data) {
     var template = document.querySelector('#picture-template');
